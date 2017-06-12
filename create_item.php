535bd2373cca2cb73df87cfa9096f4857a4d1930
Akinfemi Akin-Aluko <aakin-al@e1z2r8p11.42.us.org>
2017-06-11 19:22:30 -0700

    add item input expanded

--- create_item.php
+++ create_item.php
@@ -15,18 +15,27 @@ if ($_POST['itemid'] === "" || $_POST['price'] === "" || $_POST['submit'] !== "O
                     return ;
                 }
             }
-            $new_entry = ['itemid' => $itemid, 'price' => $price];
+            $img_path = "";
+//            if (($img_path = file_get_contents($_POST['image_path']) !== FALSE))
+//                $img_path = base64_encode($img_path);
+            $new_entry = ['itemid' => $itemid, 'price' => $price, 'item_name' => $_POST['item_name'],
+                'item_type' => $_POST['item_type'], $img_path];
             $uns_content[] = $new_entry;
             file_put_contents("../private/items", serialize($uns_content));
             header('Location: admin.html?entry=OK');
             echo "OK\n";
         }
         else{
-            $new_entry[] = ['itemid' => $itemid, 'price' => $price];
+            $img_path = "";
+//            if (($img_path = file_get_contents($_POST['image_path']) !== FALSE))
+//                $img_path = base64_encode($img_path);
+            $new_entry[] = ['itemid' => $itemid, 'price' => $price, 'item_name' => $_POST['item_name'],
+                'item_type' => $_POST['item_type'], $img_path];
             $new_entry = serialize($new_entry);
             if (file_exists("../private") === FALSE)
                 mkdir("../private", 0777);
             file_put_contents("../private/items", $new_entry);
+            header('Location: admin.html?entry=OK');
             echo "OK\n";
         }
     }
